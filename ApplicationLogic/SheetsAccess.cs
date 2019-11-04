afed725cae8c14018d7fbe454f9270af0d62b66c
camosborn21@msn.com <camosborn21@msn.com>
2019-11-04 14:55:50 -0800

    Creating Json scheme for test data

--- ApplicationLogic/SheetsAccess.cs
+++ ApplicationLogic/SheetsAccess.cs
@@ -19,58 +19,72 @@ using Newtonsoft.Json.Linq;
 
 namespace ApplicationLogic
 {
-    public class SheetsAccess
-    {
-	    public async Task Run()
-	    {
-		    //create credential
-		    UserCredential credential;
-		    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets()
-		    {
-			    ClientId = "876944923425-6cdvrf8ls90fpnf0crb9kkmnedd2ojat.apps.googleusercontent.com",
-			    ClientSecret = "6aS9xgysd8G1lXHfGEQo7XlP"
-		    }, new[] { SheetsService.Scope.Spreadsheets }, "user", CancellationToken.None);
+	public class SheetsAccess
+	{
+		const string outputFilePath = "//files.biola.edu/LibraryCircAccess/Software/Applicants/data/imports/";
+		const string ApplicantsSheetId = "1gf8fqSEYNCfr28aU_fHTvO8IED1C77MwhVmSbpoBF6c";
+		const string TestingSheetId = "1G1q3htVrxga_FEO-UvQ0BYFAli64WIBbonFym7VS0MA";
+		public async Task Run()
+		{
+			//create credential
+			UserCredential credential;
+			credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets()
+			{
+				ClientId = "876944923425-6cdvrf8ls90fpnf0crb9kkmnedd2ojat.apps.googleusercontent.com",
+				ClientSecret = "6aS9xgysd8G1lXHfGEQo7XlP"
+			}, new[] { SheetsService.Scope.Spreadsheets }, "user", CancellationToken.None);
 
-		    var service = new SheetsService(new BaseClientService.Initializer()
-		    {
-			    HttpClientInitializer = credential,
-			    ApplicationName = "Sheets API Test"
-		    });
+			var service = new SheetsService(new BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = "Sheets API Test"
+			});
 
 			//List Responses
 			//await ListSheets(service);
 			await GetApplications(service);
-	    }
+		}
 
-	    private async Task ListSheets(SheetsService service)
-	    {
-		    var sheetdata = await service.Spreadsheets.Values.Get("1G1q3htVrxga_FEO-UvQ0BYFAli64WIBbonFym7VS0MA", "A1:QN")
-			    .ExecuteAsync();
+		private async Task ListSheets(SheetsService service)
+		{
+			Console.WriteLine("Getting test data from GoogleSheets");
+			var sheetdata = await service.Spreadsheets.Values.Get(TestingSheetId, "A1:QN").ExecuteAsync();
 
-		    //get last row method
-		    Console.WriteLine(sheetdata.Values.Count);
+			JArray data = JArray.FromObject(sheetdata.Values);
 
-		    //write data
-		    Console.WriteLine(JsonConvert.SerializeObject(sheetdata.Values,Formatting.Indented));
+			Console.WriteLine("Formatting JSON data");
+			JArray result = new JArray();
+			JArray headers = data[0].ToObject<JArray>();
+			var cols = headers.Count();
+			JArray row = new JArray();
 
-	    }
+
+			//get last row method
+			Console.WriteLine(sheetdata.Values.Count);
+
+			//write data
+			Console.WriteLine(JsonConvert.SerializeObject(sheetdata.Values, Formatting.Indented));
+
+		}
 
 		private async Task GetApplications(SheetsService service)
 		{
-			const string filePath = @"//files.biola.edu/LibraryCircAccess/Software/Applicants/data/imports/applicantimports.txt";
+
+			const string applicantsFile = outputFilePath + "applicantimports.txt";
 			//get sheet data
-			var sheetData = await service.Spreadsheets.Values.Get("1gf8fqSEYNCfr28aU_fHTvO8IED1C77MwhVmSbpoBF6c", "A1:BD").ExecuteAsync();
+			Console.WriteLine("Getting Applicant data from GoogleSheets");
+			var sheetData = await service.Spreadsheets.Values.Get(ApplicantsSheetId, "A1:BD").ExecuteAsync();
 
 			JArray data = JArray.FromObject(sheetData.Values);
 			//JObject data = JObject.FromObject(sheetData.Values,new JsonSerializer());
 
 			//Console.WriteLine(data.ToString());
-
+			Console.WriteLine("Formatting JSON data");
 			JArray result = new JArray();
 			JArray headers = data[0].ToObject<JArray>();
 			var cols = headers.Count();
 			JArray row = new JArray();
-			for (var i = 1;  i< data.Count(); i++)
+			for (var i = 1; i < data.Count(); i++)
 			{
 				row = data[i].ToObject<JArray>();
 				JObject obj = new JObject();
@@ -82,37 +96,9 @@ namespace ApplicationLogic
 				result.Add(obj);
 			}
 
-			//Console.WriteLine(result.ToString());
-			System.IO.File.WriteAllText(filePath, result.ToString());
-
-			//set up
-			/*
-			var result = new IList[0];
-			var headers = sheetData.Values[0];
-			var cols = headers.Count();
-			IList row = new IList[0];
-
-			//Console.WriteLine(cols);
-
-			for(var i = 1; i < sheetData.Values.Count(); i++)
-			{
-				row = sheetData.Values[i];
-				var obj = new object { };
-				for(var col = 0; col < cols; col++)
-				{
-					obj[headers[col]] = row[col];
-				}
-			}
-			*/
-			//Console.WriteLine(sheetData.Values) ;
-
-			//data to object
-			//var serializedData = JsonConvert.SerializeObject(sheetData.Values,Formatting.Indented);
-
-			//write data
-			//Console.WriteLine(serializedData);
-
-
+			//write output file
+			Console.WriteLine(String.Format("Writing applicant data to: {0}", applicantsFile));
+			System.IO.File.WriteAllText(applicantsFile, result.ToString());
 
 		}
 
