a3c6a6c4ae94ef9b9f1dbdfb661a425aeb6f6c41
camosborn21@msn.com <camosborn21@msn.com>
2019-11-04 17:27:38 -0800

    Created Answer Key and Test Answer Download

--- ApplicationLogic/SheetsAccess.cs
+++ ApplicationLogic/SheetsAccess.cs
@@ -41,36 +41,123 @@ namespace ApplicationLogic
 			});
 
 			//List Responses
-			//await ListSheets(service);
-			await GetApplications(service);
+			await GetTestData(service);
+			//await GetApplications(service);
+			
 		}
 
-		private async Task ListSheets(SheetsService service)
+		private void TestAnswerKey()
 		{
-			Console.WriteLine("Getting test data from GoogleSheets");
-			var sheetdata = await service.Spreadsheets.Values.Get(TestingSheetId, "A1:QN").ExecuteAsync();
+			Console.WriteLine("Loading Answer Key");
+			string answerkeyFile = outputFilePath + "answerkey.json";
+			JArray answerKey = JArray.Parse(System.IO.File.ReadAllText(answerkeyFile));
+			
+			//answerKey
 
+
+
+		}
+
+
+		private async Task GetTestData(SheetsService service)
+		{
+
+
+			Console.WriteLine("Getting test data from GoogleSheets");
+			//A:QN for pre-grading
+			var sheetdata = await service.Spreadsheets.Values.Get(TestingSheetId, "A1:QN").ExecuteAsync();
 			JArray data = JArray.FromObject(sheetdata.Values);
+			
+			//Row 1 for headers
+			//Row 2 for Answer key
+			//		Column #s 0 indexed
+			//	G:EZ Name Test (6:155)
+			//	FA:KT Number Test (156:305)
+			//  KU:QN Alphabetizing Test (306:455)
+			//Row 3+ for Data
+
+			string testdataFile = outputFilePath + "testdataimports.json";
+
+
+			//Load Answer Key
+			Console.WriteLine("Loading Answer Key");
+			string answerkeyFile = outputFilePath + "answerkey.json";
+			JArray answerKey = JArray.Parse(System.IO.File.ReadAllText(answerkeyFile));
+
+
 
 			Console.WriteLine("Formatting JSON data");
 			JArray result = new JArray();
 			JArray headers = data[0].ToObject<JArray>();
-			var cols = headers.Count();
-			JArray row = new JArray();
 
+			for (var i = 2; i < data.Count(); i++)
+			{
+				//get row data
+				JArray row = data[i].ToObject<JArray>();
+				JObject test = new JObject();
+
+				test["id"] = row[0].ToString();
+
+
+				//create metadata subobject and add to test
+				JObject metadata = new JObject();
+				for (var meta = 1; meta < 6; meta++)
+				{
+					metadata[NormalizeJsonHeader( headers[meta].ToString())] = row[meta].ToString();
+				}
+				test["metadata"] = metadata;
+
+
+				JObject answers = new JObject();
+
+				//create name answers subobject and add to test
+				JObject nameAnswers = new JObject();
+				for(var names = 6; names < 156; names++)
+				{
+					nameAnswers[NormalizeJsonHeader(headers[names].ToString())] = row[names].ToString();
+				}
+				answers["name_test"] = nameAnswers;
+
+				//create number answers suboject and add to test
+				JObject numberAnswers = new JObject();
+				for(var nums = 156; nums < 306; nums++)
+				{
+					numberAnswers[NormalizeJsonHeader(headers[nums].ToString())] = row[nums].ToString();
+				}
+				answers["number_test"] = numberAnswers;
+
+				//create alpha answers subobject and add to test
+				JObject alphaAnswers = new JObject();
+				for(var alphas = 306; alphas < 456; alphas++)
+				{
+					alphaAnswers[NormalizeJsonHeader(headers[alphas].ToString())] = row[alphas].ToString();
+				}
+				answers["alphabetizing_test"] = alphaAnswers;
+				test["answers"] = answers;
+
+
+
+				//Add the formatted JSON for this test to the result array
+				result.Add(test);
+			}
+
+
+			//write output file
+			Console.WriteLine(String.Format("Writing test data to: {0}", testdataFile));
+			System.IO.File.WriteAllText(testdataFile, result.ToString());
 
 			//get last row method
-			Console.WriteLine(sheetdata.Values.Count);
+			//Console.WriteLine(sheetdata.Values.Count);
 
 			//write data
-			Console.WriteLine(JsonConvert.SerializeObject(sheetdata.Values, Formatting.Indented));
+			//Console.WriteLine(JsonConvert.SerializeObject(sheetdata.Values, Formatting.Indented));
 
 		}
 
 		private async Task GetApplications(SheetsService service)
 		{
 
-			const string applicantsFile = outputFilePath + "applicantimports.txt";
+			const string applicantsFile = outputFilePath + "applicantimports.json";
 			//get sheet data
 			Console.WriteLine("Getting Applicant data from GoogleSheets");
 			var sheetData = await service.Spreadsheets.Values.Get(ApplicantsSheetId, "A1:BD").ExecuteAsync();
@@ -100,6 +187,11 @@ namespace ApplicationLogic
 			Console.WriteLine(String.Format("Writing applicant data to: {0}", applicantsFile));
 			System.IO.File.WriteAllText(applicantsFile, result.ToString());
 
+		}
+
+		private string NormalizeJsonHeader(string text)
+		{
+			return text.ToLower().Replace(" ", "_");
 		}
 
 	}
